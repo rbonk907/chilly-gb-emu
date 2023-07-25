19b3400ec64b131ffd9f37b7bd81b8323ebf250b
r.bonk907@gmail.com <r.bonk907@gmail.com>
2023-07-24 17:22:16 -0800

    write to bus

--- bus.c
+++ bus.c
@@ -8,3 +8,11 @@ uint8_t read(uint16_t address) {
 
   return 0x00; // return No Op by default
 }
+
+void write(uint16_t address, uint8_t value) {
+  if (address >= 0x0000 && address <= 0x7FFF) {
+    rom_buffer[address] = value;
+  }
+
+  return;
+}
